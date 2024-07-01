5b72a1c4aba700fd2351e3764b6553ff8b55bd41
oscar man shrestha <oscarmanshrestha@gmail.com>
2024-07-01 17:15:51 +0545

    first commit

---
+++ day1.js
@@ -0,0 +1,28 @@
+ var name1 = "oscar shrestha"
+ var name2 = "bob marley"
+ var name3 = "saimon shrestha"
+
+// console.log(name1)
+// console.log(name2)
+// console.log(name3)
+
+//array
+var names = ['oscar','bob marley','saimon shrestha',12]
+var doesexsit = names.includes(777)
+console.log(doesexsit)
+// console.log(names.includes("oscar"))
+// console.log(names)
+// console.log(name[1])
+// console.log(name[2])
+
+//objects
+var person1 = {
+    name : "oscar shrestha",
+    age : 18,
+    address : "damak,jhapa",
+}
+// Object.freeze(person1)
+// person1.name = "racso"
+// delete parent1.address
+// console.log(person1.name,person1.age,person1.address)
+
---
+++ day3.js
@@ -0,0 +1,61 @@
+// condition in js
+// var age = "12"
+// var name = "oscar"
+// if(age==21 || name=="oscar")
+//     {
+//     console.log("you can vote")
+// }
+// else{
+// console.log("not allowed")
+// }
+// var age = 21
+// if(age === 21)
+//     {
+//     console.log("you can vote")
+// }
+// else{
+// console.log("not allowed")
+// }
+// var mahsngavakopaisa = 20
+// var sathyledinakhojekopaisa = "1000"
+// if (sathyledinakhojekopaisa == 1000){
+//     mahsngavakopaisa = mahsngavakopaisa+parseInt(sathyledinakhojekopaisa)
+// }
+//     console.log(mahsngavakopaisa)
+
+// var age = 17
+// if( age >= 17){
+//     console.log("tapi collage paddai hunuhunxa hola")
+// }
+// else if ( age > 5){
+//     console.log("tapai sschool mah hola")
+// }else {
+//     console.log ("you'r chilodren noob")
+// }
+
+
+// var isFound = true
+
+// if (isFound) {
+//     console.log( "vetiyo hai")
+// return
+// }
+// console.log("vetiyana hai")
+
+
+var name ="oscar"
+
+if (name!=="oscar") {
+    // console.log ( "name is not oscar")
+}
+
+
+
+
+
+var name ="osar"
+if (name!=="oscar"){
+    console.log("name is not oscar")
+    return
+}
+console.log("name is oscar")
